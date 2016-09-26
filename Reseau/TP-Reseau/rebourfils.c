a22be1f9def2768e613836d2f05a5891bd6b7a01
spoken <spoken.spok@gmail.com>
2016-09-26 13:22:55 +0200

    TP 1 Reseau & Com, partie II terminée et commentée

--- Reseau/TP-Reseau/rebourfils.c
+++ Reseau/TP-Reseau/rebourfils.c
@@ -1,3 +1,11 @@
+/** HECKEL Thibault - TP1 Réseaux & Com. UNIX - L3 Informatique site de Luminy | Septembre 2016
+* Partie II - 1) 2) 3)
+*
+* Note pour le correcteur :
+* Une très bonne partie de la compréhension de l'utilisation du wait c'est faite en parcourant des sujets sur le net.
+*
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,76 +14,92 @@
 
 // include .h
 #include "bor-util.h"
-#include "bor-timer.h"
 
-/*unsigned int saisir_nombre(){
-    char number[10];
-	unsigned int nombre = 0;
-    char* fin = NULL;
-
-	printf("Saisir un nombre: ");
-    fgets(number, sizeof(number), stdin);
-    nombre = strtoul(number, &fin, 10);
-
-    return nombre;
-}*/
-
-/*
-    Usage :
-
-    int h1, h2, res;
-
-    h1 = bor_timer_add (delai1, data1);
-    h2 = bor_timer_add (delai2, data2);
-
-    res = select (..., bor_timer_delay());
-
-    if (res == 0) {
-        handle = bor_timer_handle();
-        data = bor_timer_data();
-
-        if (handle == h1) {
-            printf ("delai1 ecoule");
-        } else if (handle == h2) {
-            printf ("delai2 ecoule");
-        }
-
-        bor_timer_remove (handle);
-    }
-
+/**
+* \return un entier non-signé, qui est la valeur voulue par l'utilisateur.
+*
+* Conversion d'une chaîne de caractères C entrée par l'utilisateur en un entier non-signé.
+* Cette fonction permet notamment d'arrêter le programme dans le cas où rien n'a été entré par l'utilisateur et que la touche 'enter' est préssée.
+*
+* Fonction en partie modifiée mais provenant du web.
 */
+unsigned int properScanf(){
+    char keyInput[12];
+    char* end = NULL;
+    unsigned int output = 0;
 
-int main(){
-    unsigned int number = 0;
-    //pid_t father = getpid();
-    printf("Entrez un nombre : \n");
-    /*printf("%d \n",scanf("%u",&number));*/
-    scanf("%u",&number);
+	printf("Enter a number : \n");
+    fgets(keyInput, sizeof(keyInput), stdin);
+    output = strtoul(keyInput, &end, 12);
 
-    for(unsigned int i=0; i < number; ++i){
-        if(fork()==0){
-            bor_timer_add(10);
-            //exit(0);
+    return output;
+}
+
+
+/**
+* \param : un entier non-signé à partir duquel commencer le compte à rebours.
+*
+* A chaque itération, le programme se met en pause durant une seconde.
+*
+*/
+void bombTimer(unsigned int starter){
+    for(unsigned int i = starter; i > 0; --i){
+        printf("%u - %d\n", i, getpid());       // Affichage du compte à rebours.
+        sleep(1);
+    }
+}
+
+/**
+* \param : un entier non-signé représentant le nombre de processus enfants voulus.
+*
+* Procédure permettant la création d'un nombre voulus de processus enfants.
+* Chaque enfant exécute un compteur de n secondes.
+*
+*/
+void makeChildren(unsigned int nbChild){
+    int pid;
+    for(unsigned int i = 0; i < nbChild; ++i) {
+        pid = fork();
+
+        // Utilisation d'un switch pour varié du if/else
+        switch(pid){
+            case -1:
+                perror("Erreur au niveau du fork\n");
+                exit(1);
+            break;
+
+            case 0:
+                //printf("Child (%d): %d\n", i + 1, getpid()); // Affichage du fils créé.
+                bombTimer(10);
+                exit(0);
+            break;
         }
     }
-
-    //maxValue = number;
-    /*for(unsigned int i = 0; i < number; i++) {
-        int pid = fork();
-        if(pid < 0) {
-            printf("Error");
-            exit(1);
-        } else if (pid == 0) {
-            printf("Child (%d): %d\n", i + 1, getpid());
-            exit(0);
-        } else  {
-            wait(NULL);
-        }
-    }*/
+}
 
 
+//! MAIN !//
+int main(){
+    unsigned int nbChild;   // Nombre de processus enfant voulus.
+    pid_t waitPid;          // pid qui est actuellement "attendu"
+    int waitStatus = 0;     // status qui est attendu
 
+    /*
+        Boucle Tant Que principale. Celle-ci s'exécute jusqu'à qu'un '0' soit entré par l'utilisateur.
+        Une fois 0 entré, il n'est plus possible d'intéragir avec le processus père.
+        L'utilisateur devra attendre la fin de tous les processus fils avant d'avoir la main sur la console.
+    */
+    do{
+        nbChild = properScanf();
+        makeChildren(nbChild);
+    }while(nbChild != 0);
 
-    //make_a_son(0);
+    // Tant qu'il y a un pid "attendu", nous continuons d'attendre d'autres potentiels processus.
+    // L'affection waitPid = wait(&waitStatus) est effectué dans le test de la boucle while.
+    while ((waitPid = wait(&waitStatus)) > 0);
+
+    // Une fois que tous les processus ont été attendu, affichage confirmant la fin du processus père.
+    printf("Fin de tous les fils detectes\n");
+
     return 0;
 }
